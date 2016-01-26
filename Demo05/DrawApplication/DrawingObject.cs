cb2782d066e757dabe54d8062d52ece2220d46f5
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-01-26 09:08:27 +0200

    Initial commit.

---
+++ Demo05/DrawApplication/DrawingObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawApplication
+{
+    abstract class DrawingObject
+    {
+        public string Name { get; set; }
+        public abstract void Draw();
+    }
+}
---
+++ Demo05/DrawApplication/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DrawingObject do = new DrawingObject();
+
+            Circle circle = new Circle();
+            circle.Name = "My Circle";
+            circle.Draw();
+
+            Square square = new Square();
+            square.Name = "My Square";
+            square.Draw();
+
+
+            // create a List collection for Drawing Objects
+            List<DrawingObject> drawingObjects = new List<DrawingObject>();
+            // add few objects to collection
+            drawingObjects.Add(new Circle());
+            drawingObjects.Add(new Square());
+            drawingObjects.Add(new Circle());
+            drawingObjects.Add(new Square());
+            drawingObjects.Add(new Circle());
+            drawingObjects.Add(new Square());
+            drawingObjects.Add(new Circle());
+            drawingObjects.Add(new Square());
+
+            // loop collection and draw objects
+            Console.WriteLine("Drawing objects in a List:");
+            foreach(DrawingObject drawingObject in drawingObjects)
+            {
+                drawingObject.Draw();
+            }
+
+
+            Console.WriteLine("Press enter key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
